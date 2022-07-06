f74ce0f2df5ccc0a7cf98a1e7fe83c1898882b5b
F-Rivers <francisco.rivera92@icloud.com>
2022-07-06 14:27:24 -0700

    added Text to Home

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <>
+      <h2>Welcome to the Apartment Department</h2>
+      <h3>A place to find your new place</h3>
+      </>
     )
   }
 }
